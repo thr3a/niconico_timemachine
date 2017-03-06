a721dd34c91aaa3aea08e8e2ea942dcec54735a5
thr3a <12jk172@ms.dendai.ac.jp>
2017-03-06 16:58:07 +0900

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'videos/index'
+  # get 'videos/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'videos#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
