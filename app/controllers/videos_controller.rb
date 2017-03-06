6e545ea06d03f155b0c58ff95aa644b769ebebcc
thr3a <12jk172@ms.dendai.ac.jp>
2017-03-06 21:26:27 +0900

    ランダムに表示する機能の実装

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -22,4 +22,27 @@ class VideosController < ApplicationController
       @data = JSON.parse(result.body)['data']
     end
   end
+  def random
+    query_arrays = []
+    query_arrays << (0..9).to_a
+    query_arrays << ('a'..'z').to_a
+    query_arrays << %w"、 。 / ー"
+    result = RestClient::Request.execute(
+      method: :get,
+      url: 'http://api.search.nicovideo.jp/api/v2/video/contents/search',
+      timeout: 10,
+      headers: {params: {
+        q: query_arrays.join(' OR '),
+        targets: 'title,description,tags',
+        fields: 'contentId,title,description,tags,categoryTags,viewCounter,startTime',
+        _sort: '-viewCounter',
+        _limit: 100,
+        'filters[startTime][gte]': Time.now.years_ago(10).strftime('%FT00:00:00+09:00'),
+        'filters[startTime][lt]': Time.now.years_ago(10).since(1.days).strftime('%FT00:00:00+09:00')
+      }}
+    )
+    if result.body
+      @data = JSON.parse(result.body)['data']
+    end
+  end
 end
